1295ff2121bdf59cf15a656c0f9d830c4959520e
saispandana-3 <saispandanamaddireddy3@gmail.com>
2025-04-14 14:51:42 -0400

    Update evenorodd.sh

--- evenorodd.sh
+++ evenorodd.sh
@@ -1,15 +1,18 @@
+
 #!/bin/bash
-# we are checking weather input is even or odd
-echo "enter value: "
-a=2
-y=`expr $a % 2`
-if [ $y -eq 0 ]
-then
-echo "given input $a is even"
-elif [ $y -eq 1 ]
-then
-echo "given input $a is odd"
+# We are checking whether input is even or odd
+
+a=2  # You can change this to any number
+
+# Use modern arithmetic with $((...)) instead of expr
+y=$((a % 2))
+
+if [ "$y" -eq 0 ]; then
+  echo "Given input $a is even"
+elif [ "$y" -eq 1 ]; then
+  echo "Given input $a is odd"
 else
-echo " you entered invalid number"
+  echo "You entered an invalid number"
 fi
+
 ######the end########
